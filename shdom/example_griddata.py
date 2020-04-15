deb26fc525455bd3b6c99e044d0df7ca04249e41
Jesse Loveridge <jesserl2@wirelessprv-10-194-33-5.near.illinois.edu>
2020-04-15 14:29:28 -0500

    Added shdomMicrophysics with checks and beginning of poly_table workflow.

--- shdom/example_griddata.py
+++ shdom/example_griddata.py
@@ -76,3 +76,154 @@ scatterer = scatterer.assign_coords({'x': dx * scatterer.x, 'y': dy * scatterer.
 
 scatterer.lwc.isel(x=10).plot()
 scatterer.reff.isel(x=10).plot()
+
+###New microphysics checks and _to_size_distribution here.
+
+import itertools
+
+@xr.register_dataset_accessor("shdomMicrophysics")
+class shdomMicrophysics(object):
+    def __init__(self, data_array):
+        assert len(data_array.items()) > 0, 'Dataset must be nonempty.'
+        self._obj = data_array
+
+    def _grid_checks(self):
+        """
+        TODO
+        """
+        #checks of DataArrays being 'GridData':
+        #'x','y','z' coords must be common to
+        #all variables and also consistent between variables.
+
+        grid_test = True
+        for data_var in self._obj.items():
+            for coord in ('x', 'y','z'):
+                if coord not in data_var[1].coords:
+                    grid_test = False
+                    warnings.warn('\'{}\' coord is not in {}. (x, y, z) coords must be present.'.format(data_var[0]))
+            for data_var2 in self._obj.items():
+                if not all(itertools.product(data_var[1].coords,data_var2[1].coords)):
+                    grid_test = False
+                    warnings.warn('Variables \'{}\' and \'{}\' do not have consistent coords. All coords must be consistent.'.format(data_var[0],data_var2[0]))
+        assert grid_test,'Grid Check Failed.'
+
+    #THIS IS REALLY A GENERATOR FOR DATA SHOULD COME FROM SOMEWHERE ELSE I GUESS.
+    def _add_homogeneous_like(self, reference_variable_name, variable_name, value):
+        """
+        TODO
+        """
+        self._grid_checks()
+
+        #copy first variable's coordinates etc but not any attributes.
+        to_copy = self._obj.data_vars[reference_variable_name]
+        new_data = np.full_like(to_copy, fill_value=value)
+        new_data_array = xr.DataArray(coords=to_copy.coords,data=new_data)
+        new_data_array = new_data_array.where(np.bitwise_not(np.isnan(to_copy)),np.nan)
+        self._obj[variable_name] = new_data_array
+
+
+    def microphysics_checks(self, replace_defaults=False, default_distribution_type=None, default_veff=None):
+        """
+        TODO
+        """
+        try:
+            distribution_type = self._obj.attrs['distribution_type']
+        except KeyError:
+            if replace_defaults:
+                if default_distribution_type in ('gamma','lognormal'):
+                    self._obj.attrs['distribution_type'] = default_distribution_type
+                    distribution_type = self._obj.attrs['distribution_type']
+                    warnings.warn('No \'distribution_type\' attribute specified. Replacing with default \'{}\''.format(default_distribution_type))
+                else:
+                    raise ValueError("A valid \'default_distribution_type\' must be specified. Supported values are \'gamma\' and \'lognormal\'")
+            else:
+                raise KeyError('A \'distribution_type\' attribute must be specified. Supported values are \'gamma\' and \'lognormal\'.')
+
+        if distribution_type in ('gamma','lognormal'):
+            try:
+                lwc = self._obj['lwc']
+                assert np.all(lwc.where(np.bitwise_not(np.isnan(lwc)),0.0) >=0.0), 'lwc data must be positive semidefinite.'
+            except:
+                raise
+
+            try:
+                reff = self._obj['reff']
+                assert np.all(reff.where(np.bitwise_not(np.isnan(reff)),0.0) >=0.0), 'reff data must be positive definite.'
+            except:
+                raise
+
+            try:
+                veff = self._obj['veff']
+                assert np.all(veff.where(np.bitwise_not(np.isnan(veff)),0.0) >=0.0), 'veff data must be positive definite.'
+            except KeyError:
+                try:
+                    alpha = self._obj['alpha']
+                    assert np.all(alpha.where(np.bitwise_not(np.isnan(alpha)),0.0) >=0.0), 'alpha data must be positive definite.'
+
+                except KeyError:
+                    if replace_defaults:
+                        assert default_veff > 0.0, 'default_veff must be positive definite.'
+                        self._add_homogeneous_like('lwc','veff', default_veff)
+                        warnings.warn('No \'alpha\' or \'veff\' specified for distribution_type \'{}\'. Replacing with homogeneous default value \'veff\'={}'.format(distribution_type,default_veff))
+
+                    else:
+                        raise
+        else:
+            raise NotImplementedError("Supported '\distribution_type'\ are \'gamma\' and \'lognormal\'")
+
+
+
+    def _prepare_size_distribution(self, particle_density=1.0, size_distribution_mode='GRID',max_grid_phase=1000,Ntable=50,
+                              default_distribution_type='gamma', default_veff=0.1, replace_defaults=False):
+
+        self._grid_checks()
+        self.microphysics_checks(replace_defaults=replace_defaults, default_distribution_type=default_distribution_type, default_veff=default_veff)
+
+        #in case more types are implemented I explicitly separate.
+        if self._obj.attrs['distribution_type'] in ('gamma','lognormal'):
+            reff = self._obj['reff'].values.ravel()
+            gamma=np.zeros(reff.shape)
+
+            if self._obj.attrs['distribution_type'] == 'gamma':
+                if 'veff' in self._obj:
+                    alpha = (1.0/self._obj['veff'] - 3.0).values.ravel()
+                else:
+                    alpha = self._obj['alpha'].values.ravel()
+            elif self._obj.attrs['distribution_type'] == 'lognormal':
+                if 'veff' in self._obj:
+                    alpha = np.sqrt(np.log(self._obj['veff'] + 1.0)).values.ravel()
+                else:
+                    alpha = self._obj['alpha'].values.ravel()
+
+            """
+            TODO BELOW
+            """
+            #Get Reff & Alpha coordinates for table depending whether its 'GRID' or 'TABULATED'.
+            if size_distribution_mode == 'GRID':
+
+                unique_pairs = list(set([(i,j) for i,j in zip(reff,alpha)])) #smarter way needed.
+
+                if len(unique_pairs) > max_grid_phase:
+                    warnings.warn('Number of phase functions \'{}\' is larger than suggested number {}'.format(len(unique_pairs),max_grid_phase))
+
+                #only take the unique elements.
+                reff_table, alpha_table = zip(*unique_pairs)
+
+
+            elif size_distribution_mode == 'TABULATED':
+                assert Ntable > 0
+                reff_table, alpha_table = np.meshgrid(np.arange(reff.min(),reff.max(),Ntable),
+                                                   np.arange(alpha.min(),alpha.max(),Ntable))
+
+        else:
+            raise NotImplementedError("Supported '\distribution_type'\ are \'gamma\' and \'lognormal\'")
+
+        self._obj.assign_coords({'table_reff':reff_table,
+                                 'table_alpha': alpha_table})
+
+#checks can be run independently.
+scatterer.shdomMicrophysics.microphysics_checks(replace_defaults=True,default_distribution_type='gamma',default_veff=0.1)
+
+#this definitely doesn't work. One issue is deciding on masking convention - while masked values always be np.nan?
+#what about missing, or invalid data?
+scatterer.shdomMicrophysics._prepare_size_distribution()
