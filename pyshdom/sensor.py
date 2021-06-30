Fix #11. Fix bug in exact_single_scatter gradient. Add multi-species tests. Update configuration. Fix perspective_projection, update tests to use legacy perspective_projection.

pyshdom/sensor.py
@@ -143,6 +143,11 @@ def stochastic(npixels, nrays, seed=None):
     weights = np.ones((npixels, nrays))/nrays
     return position_perturbations, weights
 
+def uniform(npixels, nrays):
+    position_perturbations = np.linspace(-1.0+1/nrays,1.0-1/nrays,nrays)
+    weights = np.ones((npixels, nrays))/nrays
+    return position_perturbations, weights
+
 def gaussian(npixels, degree):
     """
     Generates gauss-legendre weights and positions for integration of sub-pixel
@@ -438,8 +443,8 @@ def perspective_projection(wavelength, fov, x_resolution, y_resolution,
     R = np.array([nx, ny])/M # R will be used to scale the sensor meshgrid.
     dy = 2*R[1]/ny # pixel length in y direction in the normalized image plane.
     dx = 2*R[0]/nx # pixel length in x direction in the normalized image plane.
-    x_s, y_s, z_s = np.meshgrid(np.linspace(-R[0], R[0]-dx, nx),
-                                np.linspace(-R[1], R[1]-dy, ny), 1.0)
+    x_s, y_s, z_s = np.meshgrid(np.linspace(-R[0]+dx/2, R[0]-dx/2, nx),
+                                np.linspace(-R[1]+dy/2, R[1]-dy/2, ny), 1.0)
 
     # Here x_c, y_c, z_c coordinates on the image plane before transformation to the requaired observation angle
     focal = 1.0 / np.tan(np.deg2rad(fov) / 2.0) # focal (normalized) length when the sensor size is 2 e.g. r in [-1,1).

pyshdom/solver.py
@@ -327,7 +327,7 @@ class RTE:
         self._radiance, self._fluxes, self._dirflux, self._uniformzlev, \
         self._pa.extdirp, self._oldnpts, self._total_ext, self._deljdot, \
         self._deljold, self._deljnew, self._jnorm, self._work, self._work1, \
-        self._work2, ierr, errmsg, self._phaseinterpwt \
+        self._work2, ierr, errmsg, self._phaseinterpwt, self._cpu_time \
          = pyshdom.core.solution_iterations(
             verbose=verbose,
             solve=solve,
@@ -1160,7 +1160,7 @@ class RTE:
                     ((0,deriv_max_num_micro - variable_derivative.sizes['num_micro']), (0,0)),
                     mode='constant' # default pads with zeros.
                 )
-            i += 1
+                i += 1
 
         #COPIED FROM solver.RTE
         #In regions which are not covered by any optical scatterer they have an iphasep of 0.
@@ -2186,7 +2186,7 @@ class RTE:
                                      reshaped_ext[1:, :-1, 1:] + reshaped_ext[1:, :-1, :-1] + \
                                      reshaped_ext[:-1, 1:, 1:] + reshaped_ext[:-1, 1:, :-1] + \
                                      reshaped_ext[:-1, :-1, 1:] + reshaped_ext[:-1, :-1, :-1])/8.0
-        cell_volume = (np.diff(self._pa.zlevels)*self._pa.delx.data*self._pa.dely.data)**(1/3)
+        cell_volume = (np.diff(self._zgrid)*np.diff(self._xgrid.data)[0]*np.diff(self._ygrid.data)[0])**(1/3)
         cell_tau_approx = cell_volume[np.newaxis, np.newaxis, :]*cell_averaged_extinct
         number_thick_cells = np.sum(cell_tau_approx >= 2.0)
 