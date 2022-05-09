b86c02ae2de7d8e17270b4ae29b29ad22bd2d0e5
JRLoveridge <zorbaxdcat@gmail.com>
2022-05-09 13:52:51 -0500

    test.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ F2PY_MODULE_NAME = 'core'
 F2PY_SRC_PATH = 'src'
 F2PY_SIGN_FILE = '{path}/core.pyf'.format(path=F2PY_SRC_PATH)
 
-F2PY_SHDOM_FILES = ['fftpack.f', 'ocean_brdf.f', 'shdom_nompi.f', 'shdomsub5.f', 'surface.f', 'util.f90']
+F2PY_SHDOM_FILES = ['fftpack.f', 'ocean_brdf.f', 'shdom_nompi.f', 'shdomsub5.f', 'surface.f', 'util.f90' ]
 
 if POLARIZED_SHDOM:
     F2PY_SHDOM_FILES.extend(['polarized/shdom90.f90',
